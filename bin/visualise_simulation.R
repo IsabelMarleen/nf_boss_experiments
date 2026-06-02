28e4e408231881eb329abd767186c02d6e208dc8
IsabelMarleen <52419857+IsabelMarleen@users.noreply.github.com>
2026-06-02 14:09:18 +0100

    clean up visualisation script

--- bin/visualise_simulation.R
+++ bin/visualise_simulation.R
@@ -16,7 +16,6 @@ get_arguments <- function() {
   parser$add_argument('--input_unb', required = TRUE)
   parser$add_argument('--dump_time', required = TRUE)
   parser$add_argument('--pores', required = TRUE)
-  # parser$add_argument('--genome_size', required = TRUE)
   parser$add_argument('--analysed_log', required = TRUE)
   parser$add_argument('--output', required = TRUE)
   args<- parser$parse_args(commandArgs(trailingOnly = TRUE))
@@ -32,7 +31,6 @@ visualise_simulation <- function(ptol, nrow) {
   input_unb <- args$input_unb
   dump_time <- as.numeric(args$dump_time)
   pores <- as.numeric(args$pores)
-  # genome_size <- as.numeric(args$genome_size)
   log_path <- args$analysed_log
   output <- args$output
   seq_speed  <- 400
@@ -51,10 +49,6 @@ visualise_simulation <- function(ptol, nrow) {
   # load the coverage data
   cov <- read.csv(input_cov) %>%
     left_join(log_time ) %>%
-    # rename("mean_coverage_noncum" = mean_coverage,
-    #   "evenness_noncum" = evenness,
-    #   "low_coverage_prop_noncum" = low_coverage_prop) %>%
-    # mutate(mean_coverage = if_else(time==0, 0, mean_coverage_noncum))%>%
     arrange(cond, time)%>%print()
 
   
@@ -68,16 +62,6 @@ visualise_simulation <- function(ptol, nrow) {
     # reorder factor levels
     cov$bc <- factor(cov$bc, levels=bc_names)
   }
-  # if ("bc" %in% colnames(cov)){
-  #   cov <- cov %>% 
-  #   group_by(cond,bc)%>%
-  #     filter(is.na(bc))%>%
-  #     uncount(length(bc_names))%>%
-  #     mutate(bc = bc_names)%>%
-  #     bind_rows(filter(cov, !is.na(bc)))
-    
-  #   cov$bc <- factor(cov$bc, levels=bc_names)
-  # }
 
   if ("bc" %in% colnames(cov)){
     cov <- cov %>% 
@@ -87,15 +71,6 @@ visualise_simulation <- function(ptol, nrow) {
         group_by(cond)
   }
     
-  # cov <- cov %>% mutate(
-  #   seq_time = if_else(is.na(seq_time), 0, seq_time),
-  #   low_coverage_prop = if_else(time==0&low_coverage_prop_noncum==0, 1, low_coverage_prop_noncum),
-  #   mean_coverage = cumsum(mean_coverage_noncum),
-  #   low_coverage_prop = cummean(low_coverage_prop_noncum),
-  #   evenness = cummean(evenness_noncum)
-  #   )%>%print(width=Inf)
-
-  # cond, time, otu, mean_coverage, low_coverage_prop
   # get the ordering of the OTUs
   otu_order <- cov %>%
     filter(cond == 'control', time == max(time)) %>%
@@ -163,20 +138,6 @@ visualise_simulation <- function(ptol, nrow) {
     filter(cond == "boss")
 
 
-
-
-# unb_noncum <- unb_noncum %>%
-#     mutate(total = total - lag(total, default = first(total))) %>% 
-#     mutate(unb = unb - lag(unb, default = first(unb))) %>% 
-#     mutate(seq_time_noncum = seq_time - lag(seq_time, default = first(seq_time))) %>% 
-#     mutate(base_total = base_total - lag(base_total, default = first(base_total))) %>% 
-#     mutate(unb_ratio = if_else(!is.na(unb/total), unb/total, 0)) %>%
-#     mutate(total_rate = total/seq_time_noncum) #%>% #print(n=44)
-#     # summarise(seq_time=max(seq_time), total=sum(total), unb_ratio=sum(unb)/total, accept_ratio=1-unb_ratio)%>%
-#     # select(cond,seq_time, unb_ratio, accept_ratio,total) %>% 
-#     # print(n=44)
-
-
   # SANITY CHECKS ----
   # # Calculate coverage manually from all bases sequenced, not from mapping pileup, as a means of checking results
   # unb %>%
@@ -204,11 +165,6 @@ visualise_simulation <- function(ptol, nrow) {
   #   mutate(total_time = alphas + rhos + time_spent_seq, .before = dump) %>% #print()
   #   mutate(difference = dump - total_time) %>%
   #   print()
-# 630,919,767
-# 565,791,967
-
-# 560056991
-# 575991967
 
   # Coverage dotplots ----
   # load data
@@ -222,112 +178,9 @@ visualise_simulation <- function(ptol, nrow) {
   #   values_to = "cov",
   # )
 
-  # filenames_control <- list.files("temp", pattern="*.control*.csv", full.names=TRUE)
-  # ldf_control <- lapply(filenames_control, read.csv)
-  # cov_control <- Reduce(full_join, ldf_control) %>%
-  #   pivot_longer(
-  #   !V1,
-  #   cols_vary = "fastest",
-  #   names_to = "t",
-  #   values_to = "cov",
-  # )
-
-  # full_cov  <- bind_rows(cov_boss, cov_control)
-
-
 
   # PLOTS ----
-  # create plots
-
-  # unb_plot <- ggplot(
-  #     data=unb_cond,
-  #     mapping=aes(x=time, y=unb_ratio, color=otu, group=otu), alpha=0.8) +
-  #   geom_line(linewidth=1) +
-  #   geom_point() +
-  #   scale_color_manual(values=ptol) +
-  #   ylab("cum. rejection rate") +
-  #   ylim(0,1)+
-  #   xlim(0, max(unb$seq_time))+
-  #   theme(legend.position = "none")
-  # # unb_plot
-
-  # unb_noncum_plot <- ggplot(
-  #     data=filter(unb_noncum,cond == "boss"),
-  #     mapping=aes(x=time, y=unb_ratio, color=otu, group=otu)) +
-  #   geom_line(linewidth=1) +
-  #   geom_point() +
-  #   scale_color_manual(values=ptol) +
-  #   ylab("rejection rate") +
-  #   ylim(0,1)+
-  #   xlim(0, max(unb$seq_time))+
-  #   theme(legend.position = "none")
-  # # unb_plot
-
-  # nreads <- ggplot(
-  #     data=unb,
-  #     mapping=aes(x=time, y=total, linetype=cond, colour=otu)) +
-  #   geom_line(linewidth=1) +
-  #   facet_wrap(~otu, scales="free_y", nrow = nrow) +
-  #   scale_color_manual(values=ptol, guide = "none") +
-  #   ylab("# reads")
-  # # nreads
-
-  # nreads_noncum <- ggplot(
-  #    unb_noncum,
-  #     # data=mutate(unb_noncum, total_rate = if_else(time == 0, NA, total_rate)),
-  #     mapping=aes(x=time, y=total_rate, linetype=cond, colour=otu)) +
-  #   geom_line(linewidth=1) +
-  #   facet_wrap(~otu, scales="free_y", nrow = nrow) +
-  #   scale_color_manual(values=ptol, guide = "none") +
-  #   ylab("# reads/time unit")
-  # # nreads
-
-  # meanc <- ggplot(
-  #     data=cov,
-  #     mapping=aes(x=time, y=mean_coverage, linetype=cond, colour=otu)) +
-  #   geom_line(linewidth=1) +
-  #   facet_wrap(~otu, scales="free_y", nrow = nrow) +
-  #   scale_color_manual(values=ptol, guide = "none") +
-  #   ylab("mean coverage")
-  # # meanc
-
-  # lowc <- ggplot(
-  #     data=cov,
-  #     mapping=aes(x=time, y=low_coverage_prop, linetype=cond, colour=otu)) +
-  #   geom_line(linewidth=1) +
-  #   facet_wrap(~otu, scales="free_y", nrow = nrow) +
-  #   scale_color_manual(values=ptol, guide = "none") +
-  #   ylab("prop. sites at <5x")
-  # # lowc
-
-  # evn <- ggplot(
-  #     data=cov,
-  #     mapping=aes(x=time, y=evenness, linetype=cond, colour=otu)) +
-  #   geom_line(linewidth=1) +
-  #   facet_wrap(~otu, scales="free_y", nrow = nrow) +
-  #   scale_color_manual(values=ptol, guide = "none") +
-  #   ylab("evenness")
-  # # evenness of coverage
-
-  # time_frag <- ggplot(
-  #     data=unb,
-  #     mapping=aes(x=total, y=time, linetype=cond, colour=otu)) +
-  #   geom_line(linewidth=1) +
-  #   facet_wrap(~otu, scales="free_y", nrow = nrow) +
-  #   scale_color_manual(values=ptol, guide = "none") +
-  #   ylab("seq. time (intervals)")
-  # # nreads
-
-  # evn_noncum <- ggplot(
-  #     data=mutate(cov_noncum),
-  #     mapping=aes(x=time, y=evenness, linetype=cond, colour=otu)) +
-  #   geom_line(linewidth=1) +
-  #   facet_wrap(~otu, scales="free_y", nrow = nrow) +
-  #   scale_color_manual(values=ptol, guide = "none") +
-  #   ylab("evenness")
-  # evenness of coverage
-
-  # create plots with new attempt at scaling time units to real sequencing time
+  # create plots 
    if ("bc" %in% colnames(cov)){
      col <- "bc"
   } else {
@@ -347,7 +200,7 @@ visualise_simulation <- function(ptol, nrow) {
     scale_shape_manual(values=c(21,22,23), guide = "none")+
     ylab("cum. rejection rate") +
     xlim(0, max(unb_cond$seq_time))+
-    facet_wrap(~otu, scales="free_y", nrow = nrow) +
+    # facet_wrap(~otu, scales="free_y", nrow = nrow) +
     ylim(0,1)+
     theme(legend.position = "none")
   # unb_plot
@@ -407,10 +260,8 @@ visualise_simulation <- function(ptol, nrow) {
 
   nreads_noncum_seq <- ggplot(
       data=mutate(unb, total_rate = if_else(time == 0, NA, total_rate)),
-      # mapping=aes(x=seq_time, y=total_rate, linetype=cond, colour=bc)) +
             mapping=aes(x=seq_time, y=total_rate, linetype=cond, colour=.data[[col]])) +
     geom_line()+
-    # geom_borderline() +
     facet_wrap(~otu, scales="free_y", nrow = nrow) +
     xlim(0, max(unb_cond$seq_time))+
     scale_color_manual(values=ptol) +
@@ -421,9 +272,7 @@ visualise_simulation <- function(ptol, nrow) {
 
   meanc_seq <- ggplot(
       data=cov,
-      # mapping=aes(x=seq_time, y=mean_coverage, linetype=cond, colour=bc)) +
             mapping=aes(x=seq_time, y=mean_coverage, linetype=cond, colour=.data[[col]], linewidth=.data[[col]], alpha=.data[[col]])) +
-
     geom_line() +
     facet_wrap(~otu, scales="free_y", nrow = nrow) +
     xlim(0, max(unb_cond$seq_time))+
@@ -439,14 +288,11 @@ visualise_simulation <- function(ptol, nrow) {
 
   lowc_seq <- ggplot(
       data=cov,
-      # mapping=aes(x=seq_time, y=low_coverage_prop, linetype=cond, colour=bc)) +
             mapping=aes(x=seq_time, y=low_coverage_prop, linetype=cond, colour=.data[[col]], linewidth=.data[[col]])) +
-
     geom_line() +
     facet_wrap(~otu, scales="free_y", nrow = nrow) +
     scale_color_manual(values=ptol) +
     xlim(0, max(unb_cond$seq_time))+
-    # geom_borderline()+
     scale_linewidth_manual(values=c(1,0.8,0.6))+
     ylab("prop. sites at <5x")+
     theme(legend.position = "none")
@@ -455,9 +301,7 @@ visualise_simulation <- function(ptol, nrow) {
 
   evn_seq <- ggplot(
       data=cov,
-      # mapping=aes(x=seq_time, y=evenness, linetype=cond, colour=bc)) +
             mapping=aes(x=seq_time, y=evenness, linetype=cond, colour=.data[[col]], linewidth=.data[[col]], alpha=cond)) +
-
     geom_line() +
     facet_wrap(~otu, scales="free_y", nrow = nrow) +
     scale_color_manual(values=ptol) +
@@ -473,9 +317,7 @@ visualise_simulation <- function(ptol, nrow) {
 
   time_frag_seq <- ggplot(
       data=unb,
-      # mapping=aes(x=total, y=seq_time, linetype=cond, colour=bc)) +
             mapping=aes(x=total, y=seq_time, linetype=cond, colour=.data[[col]], linewidth=.data[[col]])) +
-
     geom_line() +
     scale_linewidth_manual(values=c(1,0.8,0.6))+
     facet_wrap(~otu, scales="free_y", nrow = nrow) +
@@ -483,32 +325,6 @@ visualise_simulation <- function(ptol, nrow) {
     ylab("seq. time (minutes)")
   # nreads
   ggsave(output, time_frag_seq, w=4, h=7/3)
-
-  # layout <- ({unb_plot + unb_noncum_plot} / {nreads + nreads_noncum} / {meanc + plot_spacer()} / {lowc + time_frag} / {evn + plot_spacer()} / guide_area()) +
-  #   plot_annotation(tag_levels = "A") +
-  #   plot_layout(guides="collect", heights=rbind(c(1, 1, 1, 1, 1, 0.2),c(1, 1, NA, NA, NA, NA))) &
-  #   xlab("seq. time (intervals)") &
-  #   theme(
-  #     legend.position = "bottom",
-  #     legend.title = element_blank(),
-  #     strip.text.x = element_blank(),
-  #     plot.background = element_rect(fill = "white", color = NA)
-  #   )
-
-  # layout[[4]] <- layout[[4]] + xlab("# reads")
-
-  # ggsave(output, layout, w=8, h=11)
-
-  # layout_seqt <- ({unb_plot_seq + unb_noncum_plot_seq} / {nreads_seq + nreads_noncum_seq} / {meanc_seq + time_frag_seq} / {lowc_seq + plot_spacer()} / {evn_seq + plot_spacer()}/ guide_area()) +
-  #   plot_annotation(tag_levels = "A") +
-  #   plot_layout(guides="collect", heights=rbind(c(1, 1, 1, 1, 1, 0.2),c(1, 1, 1, NA, NA, NA))) &
-  #   xlab("seq. time (minutes)") &
-  #   theme(
-  #     legend.position = "bottom",
-  #     legend.title = element_blank(),
-  #     strip.text.x = element_blank(),
-  #     plot.background = element_rect(fill = "white", color = NA)
-  #   )
   
   layout_seqt <- wrap_plots(list(unb_plot_seq, unb_plot_frag, nreads_seq,lowc_seq, meanc_seq, evn_seq), axes='keep', axis_titles="keep")+
     plot_annotation(tag_levels = "A") +
@@ -521,7 +337,6 @@ visualise_simulation <- function(ptol, nrow) {
     theme(
       legend.position = "bottom",
       legend.title = element_blank(),
-      # strip.text.x = element_blank(),
       plot.background = element_rect(fill = "white", color = NA)
     )
 
