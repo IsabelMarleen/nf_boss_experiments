33c4e139706b48d16104c3a493cf96d9ffadd3e2
IsabelMarleen <52419857+IsabelMarleen@users.noreply.github.com>
2026-06-02 14:09:45 +0100

    add new script for visualising exp with many otu

---
+++ bin/visualise_many_chr.R
@@ -0,0 +1,285 @@
+#!/usr/bin/env Rscript
+
+library(ggplot2)
+library(argparse)
+library(dplyr)
+library(patchwork)
+library(stringr)
+library(tidyr)
+library(colorspace)
+
+theme_set(theme_minimal())
+
+
+get_arguments <- function() {
+  parser <- argparse::ArgumentParser()
+  parser$add_argument('--input_cov', required = TRUE)
+  parser$add_argument('--input_unb', required = TRUE)
+  parser$add_argument('--dump_time', required = TRUE)
+  parser$add_argument('--pores', required = TRUE)
+  parser$add_argument('--analysed_log', required = TRUE)
+  parser$add_argument('--output', required = TRUE)
+  args<- parser$parse_args(commandArgs(trailingOnly = TRUE))
+  return(args)
+}
+
+
+visualise_simulation <- function(ptol, nrow) {
+  # ARGUMENTS ----
+  # get the arguments for input and output files
+  args <- get_arguments()
+  input_cov <- args$input_cov
+  input_unb <- args$input_unb
+  dump_time <- as.numeric(args$dump_time)
+  pores <- as.numeric(args$pores)
+  log_path <- args$analysed_log
+  output <- args$output
+  seq_speed  <- 400
+
+  # ANALYSED LOG ----
+  # load the analysed log
+  log_file  <- read.csv(log_path)
+
+  # extract times for dumps
+  log_time  <- log_file %>%
+    group_by(cond, dump) %>% 
+    summarise(time=last(time)) %>%
+    rename(seq_time = time, time = dump)
+ 
+  # COVERAGE ----
+  # load the coverage data
+  cov <- read.csv(input_cov) %>%
+    left_join(log_time ) %>%
+    arrange(cond, time)
+
+  
+  if ("bc" %in% colnames(cov)){
+    bc_names  <- cov %>%
+      filter(bc != "0.0")%>%
+      arrange(bc)%>%
+      pull(bc)%>%
+      unique()
+    
+    # reorder factor levels
+    cov$bc <- factor(cov$bc, levels=bc_names)
+  }
+
+  if ("bc" %in% colnames(cov)){
+    cov <- cov %>% 
+        group_by(cond,bc)
+  } else {
+    cov <- cov %>% 
+        group_by(cond)
+  }
+    
+  # get the ordering of the OTUs based on mean coverage
+  otu_order <- cov %>%
+    filter(cond == 'control', time == max(time)) %>%
+    arrange(desc(mean_coverage)) %>%
+    pull(otu)%>%
+    unique()
+  # reorder factor levels
+  cov$otu <- factor(cov$otu, levels=otu_order)
+
+
+  # convert time units to pseudominutes
+  cov <- cov %>%
+    mutate(seq_time = if_else(is.na(seq_time), 0, seq_time/seq_speed/pores/60),
+            evenness = if_else(seq_time==0, NA, evenness))
+
+
+  # UNBLOCK ----
+  # load the unblocking data and reorder the factor levels
+  unb <- read.csv(input_unb) %>% 
+    left_join(log_time) %>% 
+    mutate(orig_time = seq_time) %>%
+    mutate(seq_time = if_else(is.na(seq_time), 0, seq_time))%>%
+    arrange(cond, time)%>%print()
+  unb$otu <- factor(unb$otu, levels=otu_order)
+
+  # convert time units to minutes
+  unb <- unb %>%
+    mutate(seq_time = seq_time/seq_speed/pores/60)
+
+  if ("bc" %in% colnames(cov)){
+    unb <- unb %>% 
+      group_by(cond, otu)%>%
+      filter(bc=="0")%>%
+      uncount(length(bc_names))%>%
+      mutate(bc = bc_names)%>%
+      bind_rows(filter(unb, bc!="0"))%>%
+      mutate(orig_time = if_else(is.na(orig_time), 0, orig_time),
+       unb_ratio = if_else(is.na(unb_ratio), 0, unb_ratio)) %>%
+       arrange(cond,time,bc)%>%print()
+
+    # reorder factor levels
+    unb$bc <- factor(unb$bc, levels=bc_names)
+
+    unb <- unb %>% 
+      group_by(cond, bc, otu)
+  }
+  else{
+    unb <- unb %>%
+        group_by(cond,otu)
+  }
+
+  unb <- unb %>%
+    rename("total_noncum" = total, "base_total_noncum" = base_total, "unb_noncum" = unb) %>%
+    mutate(total = cumsum(total_noncum), 
+      base_total = cumsum(as.numeric(base_total_noncum)),
+      unb = cumsum(unb_noncum),
+      seq_time_noncum = seq_time - lag(seq_time, default = first(seq_time)),
+      unb_ratio = if_else(!is.na(unb/total), unb/total, 0),
+      unb_ratio_noncum = if_else(!is.na(unb_noncum/total_noncum), unb_noncum/total_noncum, 0),
+      total_rate = total/seq_time_noncum)
+
+  unb_cond <- unb %>% 
+    filter(cond == "boss")
+
+  # PLOTS ----
+  # create plots 
+  # create plots with new attempt at scaling time units to real sequencing time
+num_otu <- unb_cond%>%pull(otu)%>%unique()%>%length()
+num_bc <- unb_cond%>%pull(bc)%>%unique()%>%length()
+colours <- list()
+
+for (a in 1:num_bc){
+  light = lighten(
+    ptol[a],
+    amount = 0.5,
+    method = c("relative"),
+    fixup = TRUE
+  )
+  dark = darken(
+    ptol[a],
+    amount = 0.5,
+    method = c("relative"),
+    fixup = TRUE
+  )
+  colfunc <- colorRampPalette(c(dark, light))
+  colvals <- colfunc(num_otu)
+  colours <- append(colours, colvals)
+}
+colours  <- unlist(colours)
+  
+  unb_plot_seq <- ggplot(
+      data=arrange(unb_cond, bc),
+      mapping=aes(x=seq_time, y=unb_ratio)) +
+    geom_line(aes(group=interaction(otu,bc),colour=interaction(otu,bc)),linewidth=0.1,alpha=0.7) +
+    scale_color_manual(values=colours)+
+    ylab("cum. rejection rate") +
+    xlim(0, max(unb_cond$seq_time))+
+    ylim(0,1)+
+    guides(colour = guide_legend(override.aes = list(linewidth = 20)))
+  # unb_plot
+ ggsave(output, unb_plot_seq, w=4, h=15)
+
+   unb_plot_frag <- ggplot(
+      data=unb_cond,
+      mapping=aes(x=total, y=unb_ratio)) +
+    geom_line(aes(group=interaction(otu,bc),colour=interaction(otu,bc)),size=0.1,alpha=0.7)+
+    scale_color_manual(values=colours, guide = "none") +
+    ylab("cum. rejection rate") +
+    xlim(0, max(unb_cond$total))+
+    ylim(0,1)+
+    theme(legend.position = "none")
+  # unb_plot
+ ggsave(output, unb_plot_frag, w=4, h=7/3)
+
+  nreads_seq <- ggplot(
+      data=unb,
+            mapping=aes(x=seq_time, y=total, linetype=cond, colour=interaction(otu,bc))) +
+    geom_line(alpha=0.8, linewidth=0.1) +
+    xlim(0, max(unb_cond$seq_time))+
+    scale_color_manual(values=colours, guide = "none") +
+    ylab("# fragments")+
+    guides(linetype = guide_legend(override.aes = list(linewidth = 0.5)))
+  # nreads
+  ggsave(output, nreads_seq, w=4, h=7/3)
+ 
+
+  nreads_noncum_seq <- ggplot(
+      data=mutate(unb, total_rate = if_else(time == 0, NA, total_rate)),
+            mapping=aes(x=seq_time, y=total_rate, linetype=cond, colour=interaction(otu,bc))) +
+    geom_line(alpha=0.8, linewidth=0.1)+
+    xlim(0, max(unb_cond$seq_time))+
+    scale_color_manual(values=colours, guide = "none") +
+    ylab("# fragments/min")+
+    theme(legend.position = "none")
+  # nreads
+  ggsave(output, nreads_noncum_seq, w=4, h=7/3)
+
+  meanc_seq <- ggplot(
+      data=cov,
+            mapping=aes(x=seq_time, y=mean_coverage, linetype=cond, colour=interaction(otu,bc))) +
+    geom_line(alpha=0.8, linewidth=0.1) +
+    xlim(0, max(unb_cond$seq_time))+
+    ylim(0, max(pull(slice_min(filter(cov, seq_time > max(unb_cond$seq_time)), order_by=time), mean_coverage)))+
+    scale_color_manual(values=colours, guide = "none") +
+    ylab("mean coverage")+
+    theme(legend.position = "none")
+  # meanc
+  ggsave(output, meanc_seq, w=4, h=7/3)
+
+
+  lowc_seq <- ggplot(
+      data=cov,
+            mapping=aes(x=seq_time, y=low_coverage_prop, linetype=cond, colour=interaction(otu,bc))) +
+    geom_line(alpha=0.8, linewidth=0.1) +
+    scale_color_manual(values=colours, guide = "none") +
+    xlim(0, max(unb_cond$seq_time))+
+    scale_linewidth_manual(values=c(1,0.8,0.6))+
+    ylab("prop. sites at <5x")+
+    theme(legend.position = "none")
+  # lowc
+  ggsave(output, lowc_seq, w=4, h=7/3)
+
+  evn_seq <- ggplot(
+      data=cov,
+            mapping=aes(x=seq_time, y=evenness, linetype=cond, colour=interaction(otu,bc))) +
+    geom_line(alpha=0.8, linewidth=0.1) +
+    scale_color_manual(values=colours, guide = "none") +
+    xlim(0, max(unb_cond$seq_time))+
+    ylab("evenness")+
+    theme(legend.position = "none")
+  # evenness of coverage
+  ggsave(output, evn_seq, w=4, h=7/3)
+
+
+
+  time_frag_seq <- ggplot(
+      data=unb,
+            mapping=aes(x=total, y=seq_time, linetype=cond, colour=.data[[col]], linewidth=.data[[col]])) +
+    geom_line() +
+    scale_linewidth_manual(values=c(1,0.8,0.6))+
+    scale_color_manual(values=ptol)+#, guide = "none") +
+    ylab("seq. time (minutes)")
+  # nreads
+  ggsave(output, time_frag_seq, w=4, h=7/3)
+
+  layout_seqt <- wrap_plots(list(unb_plot_seq, unb_plot_frag, nreads_seq,lowc_seq, meanc_seq, evn_seq), axes='keep', axis_titles="keep")+
+    plot_annotation(tag_levels = "A") +
+    plot_layout(
+      nrow = 3,
+      ncol = 2,
+      guides="collect"
+      ) &
+    xlab("seq. time (pseudominutes)") &
+    theme(
+      legend.position = "bottom",
+      legend.title = element_blank(),
+      plot.background = element_rect(fill = "white", color = NA)
+    )
+
+  layout_seqt[[2]] <- layout_seqt[[2]] + xlab("# fragments")
+  ggsave(output, layout_seqt, w=8, h=7)
+
+}
+
+
+ptol <- c("#CC6677","#332288","#DDCC77","#117733","#88CCEE","#882255","#44AA99","#999933","#AA4499")
+nrow <- 1
+visualise_simulation(ptol, nrow)
+
+
+
