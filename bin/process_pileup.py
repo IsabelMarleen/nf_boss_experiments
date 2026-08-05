41cdcdcc25c145f69e474f4764f88919d9e41157
IsabelMarleen <52419857+IsabelMarleen@users.noreply.github.com>
2026-08-05 14:03:20 +0100

    lint and adjust for merged bam files

--- bin/process_pileup.py
+++ bin/process_pileup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-import numpy as np
-import scipy.integrate as integrate
 import subprocess
 import sys
-
 from os import path
+
+import numpy as np
+from scipy import integrate
+
 PARENT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
 sys.path.append(PARENT_DIR)
 sys.path.append('.')
@@ -14,61 +15,24 @@ from otu_info import genome_sizes   # noqa
 
 
 
-def parse_pup(pup_path: str, pup_full:str, count: int) -> np.array:
+def parse_pup(pup_path: str) -> np.array:
     '''
     Parse the pileup file to extract the coverage values as an array
     :param pup_path: path to the pileup file
     :return: array of coverage
     '''
-    # determine which columns need to be extracted
-    # 2 is the position, then the 4th column is the first coverage and then there are two columns in-between all the remaining coverages
-    col_string = "2,"+','.join(map(str, range(4, 4+(3*count), 3)))
-
-    # extract all relevant columns and output with comma separation
-    running2 = subprocess.Popen(
-        args=f"cut -f{col_string} --output-delimiter=',' {pup_path} > tmp.csv",
+    # extract 4th column only
+    running = subprocess.Popen(
+        args=f"cut -f4 {pup_path}",
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         encoding='utf-8',
         shell=True
     )
-    stdout2, stderr2 = running2.communicate()
-
-    cov = np.loadtxt("tmp.csv", delimiter=",", dtype="int")
-    # stdout4_interim = [subprocess.Popen(
-    #     args=f"cut -f{i} {pup_path}",
-    #     stdout=subprocess.PIPE,
-    #     stderr=subprocess.PIPE,
-    #     encoding='utf-8',
-    #     shell=True
-    # ) for i in range(4, 4+(3*count), 3)]
-    # stdout4 = [[j for j in j.communicate()[0].split("\n") if j] for j in stdout4_interim ]
-
-    # [[subprocess.Popen(
-    #     args=f"cut -f{i} {pup_path}",
-    #     stdout=subprocess.PIPE,
-    #     stderr=subprocess.PIPE,
-    #     encoding='utf-8',
-    #     shell=True
-    # ) for i in range(4, 4+(3*count), 3)] for j in j.communicate()[0].split("\n") if j]
-    
-
-    # running4 = subprocess.Popen(
-    #     args=f"cut -f4 {pup_path}",
-    #     stdout=subprocess.PIPE,
-    #     stderr=subprocess.PIPE,
-    #     encoding='utf-8',
-    #     shell=True
-    # )
-
-    # stdout4, stderr4 = running4.communicate()
+    stdout, _stderr = running.communicate()
     # parse to array of coverage
-    # pos = np.array([i for i in stdout2.split("\n") if i], dtype="int")
-    # coverage = np.array(stdout4, dtype="int")
-
-    # cov = np.array([pos, coverage], dtype="int")
-    
-    return cov #.transpose()
+    cov = np.array([i for i in stdout.split("\n") if i], dtype="int")
+    return cov
 
 
 def calc_lowcov(cov: np.array, otu_size: int, lowcov_threshold: int = 5) -> float:
@@ -80,8 +44,7 @@ def calc_lowcov(cov: np.array, otu_size: int, lowcov_threshold: int = 5) -> floa
     :param otu_size: genome size
     :return: proportion of sites with low coverage
     '''
-    # lowcov_n = np.where(cov < lowcov_threshold)[0].shape[0]
-    lowcov_n = np.sum(cov < lowcov_threshold, axis=0)
+    lowcov_n = np.where(cov < lowcov_threshold)[0].shape[0]
     # to account for sites without coverage
     # calc difference in genome length and cov array
     nocov_n = otu_size - cov.shape[0]
@@ -100,20 +63,20 @@ def calc_evenness(cov: np.array, otu_size: int) -> float:
     :return: evenness of coverage
     '''
     def _calc_evn(i, cov, otu_size) -> float:
-        avg_cov = np.average(cov, axis=0)
+        avg_cov = np.average(cov)
         cov_norm = cov/avg_cov
         p_i = np.count_nonzero(cov_norm >= (i/avg_cov))
 
         evn = p_i/otu_size
         return evn
 
-    evenness = [integrate.quad(_calc_evn, 0, 1, args=(cov[:,i], otu_size))[0] for i in range(0, cov.shape[1])]
+    evenness = integrate.quad(_calc_evn, 0, 1, args=(cov, otu_size))[0]
     
     return evenness
 
 
 
-def process_pup(pup: str, otu_size: int, pup_full: str, count: int):
+def process_pup(pup: str, otu_size: int, pup_full: str):
     '''
     Process the pileup file to extract the mean coverage and proportion of low coverage sites
     :param pup: path of pileup file
@@ -122,37 +85,28 @@ def process_pup(pup: str, otu_size: int, pup_full: str, count: int):
     '''
     # get metadata
     meta = pup.split("/")[-1].split(".")[0].split('_')
-    if len(meta) > 2:
-        cond, bc, otu = meta[0], meta[1], meta[2]
+    if len(meta) > 3:
+        cond, time, bc, otu = meta[0], meta[1], meta[2], meta[3]
     else:
-        cond, otu = meta[0], meta[1]
+        cond, time, otu = meta[0], meta[1], meta[2]
     # get an array of coverage from the pileup file
-    cov = parse_pup(pup_path=pup, pup_full=pup_full, count=count)
-    cov_cumsum = np.empty_like(cov[:,1:], dtype="int")
-    cov[:, 1:].cumsum(axis=1, out=cov_cumsum)
-    print("cumsum complete")
-    # np.savetxt(pup_full, cov_cumsum)
-    # pup_full_2 = "/hps/nobackup/goldman/ipoetzsch/boss_experiments/work/de/87802f7c6910861995d08dc607c94f/hschr21"
-    # cov = np.loadtxt(pup_full)
+    cov = parse_pup(pup_path=pup)
     # calculate the mean coverage of the OTU
-    # mean_cov = np.sum(cov[:, 1:], axis=0) / otu_size
-    mean_cov = np.sum(cov_cumsum, axis=0) / otu_size
+    mean_cov = np.sum(cov) / otu_size
     # calculate the proportion of low coverage sites
-    # lowcov_p = calc_lowcov(cov=cov[1], otu_size=otu_size)
-    lowcov_p = calc_lowcov(cov=cov_cumsum, otu_size=otu_size)
-
+    lowcov_p = calc_lowcov(cov=cov, otu_size=otu_size)
     # calculate the evenness of coverage of the OTU
-    evenness = calc_evenness(cov=cov_cumsum, otu_size=otu_size)
+    evenness = calc_evenness(cov=cov, otu_size=otu_size)
+
     with open(pup_full, "w") as output:
         if len(meta) > 2:
-            output.write(f'{cond},0,{otu},{bc},0,1,0\n')
-            for i in range(1, count):
-                output.write(f'{cond},{i},{otu},{bc},{mean_cov[i]},{lowcov_p[i]},{evenness[i]}\n')
+            if time == 1:
+                output.write(f'{cond},0,{otu},{bc},0,1,0\n')
+            output.write(f'{cond},{time},{otu},{bc},{mean_cov},{lowcov_p},{evenness}\n')
         else:
-            output.write(f'{cond},0,{otu},0,1,0\n')
-            for i in range(1, count):
-                output.write(f'{cond},{i},{otu},{mean_cov[i]},{lowcov_p[i]},{evenness[i]}\n')
-    return
+            if time == 1:
+                output.write(f'{cond},0,{otu},0,1,0\n')
+            output.write(f'{cond},{time},{otu},{mean_cov},{lowcov_p},{evenness}\n')
 
 
 
@@ -161,9 +115,9 @@ if __name__ == "__main__":
     # input to this script is the output of samtools pileup
     # also pass in the genome size of the OTU to calculate the mean coverage
     otu = sys.argv[2]
-    otu_size = (lambda w: genome_sizes[w])(otu)
+    otu_size = genome_sizes[otu]
 
-    process_pup(pup=sys.argv[1], otu_size=otu_size, pup_full=sys.argv[3], count=int(sys.argv[4]))
+    process_pup(pup=sys.argv[1], otu_size=otu_size, pup_full=sys.argv[3])
 
 
 
--- modules/local/pileup.nf
+++ modules/local/pileup.nf
@@ -1,23 +1,14 @@
 process pileup {
-    tag "${meta.join("_")}"
+    tag "${meta.id}"
     input:
-        tuple val(meta), path(bam)
-        tuple val(meta2), path(bai)
+        tuple val(meta), path(bam), path(bai)
         file otu_file
     output: 
-        path 'tmp.csv', emit: pup
-        path "${meta.join("_")}.csv", emit: csv
+        path "${meta.id}.csv", emit: csv
     script:
-    if (params.barcodes == null){
-        otu = meta.get(1)
-        assert otu == meta2.get(1)
-    }
-    else{
-        otu = meta.get(2)
-        assert otu == meta2.get(2)
-    }
+    otu = bam.simpleName.split(/_/)[-2]
     """
-    samtools mpileup -Q 0 ${bam} > ${meta.join("_")}.pup &&
-    process_pileup.py ${meta.join("_")}.pup $otu ${meta.join("_")}.csv ${bam.size()}
+    samtools mpileup -Q 0 ${bam} > ${meta.id}.pup &&
+    process_pileup.py ${meta.id}.pup $otu ${meta.id}.csv
     """
 }
